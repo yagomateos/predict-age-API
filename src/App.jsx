468ef1466588e40fb12690a7ae05380f472da918
Yago <yagomateos@hotmail.com>
2023-04-12 12:29:14 +0200

    updated

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css'
+import Axios from 'axios';
+import { useEffect, useState } from 'react'
+
+function App() {
+  const [name, setName] = useState("");
+  const [predictedAge, setPredictedAge] = useState(null);
+
+  const fetchData = () => {
+    Axios.get(`https://api.agify.io?name=${name}`).then((res) => {
+      setPredictedAge(res.data);
+    });
+  };
+
+  return (
+    <div className="App">
+    <input
+    placeholder="Ex: Yago..."
+    onChange={(event) => {
+      setName(event.target.value);
+    }}
+    /><br></br><br></br>
+      <button onClick={fetchData}>Predict Age</button>
+      <h1>Name: {predictedAge?.name}</h1>
+      <h1>Predicted Age: {predictedAge?.age}</h1>
+      <h1>Count: {predictedAge?.count}</h1>
+    </div>
+  )
+}
+
+export default App
